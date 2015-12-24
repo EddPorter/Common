34a7e7e9e85759ed209750896b8732df2989c6c2
Edd Porter <git@eddporter.com>
2015-12-24 22:36:11 +0000

    Create initial "Common" project

---
+++ EddPorter.Common/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EddPorter.Common")]
+[assembly: AssemblyDescription("Collection of useful and reusable code snippets.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Edd Porter")]
+[assembly: AssemblyProduct("EddPorter.Common")]
+[assembly: AssemblyCopyright("Copyright ©Edd Porter 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: NeutralResourcesLanguage("en-GB")]
